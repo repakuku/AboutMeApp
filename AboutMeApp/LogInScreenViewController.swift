ea9c96773ac02c864a51d6072db13e2cba538df3
Alexey Turulin <repakuku@icloud.com>
2023-05-10 14:24:44 -0400

    Interface is done

--- AboutMeApp/LogInScreenViewController.swift
+++ AboutMeApp/LogInScreenViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class LogInScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ WelcomeScreenViewController.swift
@@ -0,0 +1,12 @@
+//
+//  WelcomeScreenViewController.swift
+//  AboutMeApp
+//
+//  Created by Алексей Турулин on 5/10/23.
+//
+
+import UIKit
+
+class WelcomeScreenViewController: UITabBarController {
+
+}
